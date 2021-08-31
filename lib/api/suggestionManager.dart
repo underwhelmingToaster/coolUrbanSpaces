a312bfbc6d281809aad30b0d6bf7ef37b17f017d
Fabio Zahner <zahnerfabio@gmail.com>
2021-08-31 17:26:26 +0200

    tried to implement icons but they don't display

--- lib/api/suggestionManager.dart
+++ lib/api/suggestionManager.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:latlong2/latlong.dart';
 
 class SuggestionManager {
   List getSuggestions () {
-    var suggestion1 = new Suggestion("titel 1", "text 1", "type 1", 51.1, -0.09);
-    var suggestion2 = new Suggestion("titel 2", "text 2", "type 2", 51.2, -0.07);
+    var suggestion1 = new Suggestion("titel 1", "text 1", 1, 51.1, -0.09);
+    var suggestion2 = new Suggestion("titel 2", "text 2", 2, 51.2, -0.07);
     var suggestions = [suggestion1, suggestion2];
     return suggestions;
   }
@@ -15,7 +16,17 @@ class SuggestionManager {
     List<Marker> markers = [];
     var suggestions = getSuggestions();
     for (int i = 0; i < suggestions.length; i++){
-      if()
+      Widget icon = SvgPicture.asset("icons/shade.svg");
+      switch(suggestions[i].type) {
+        case 1: icon = SvgPicture.asset("../icons/shade.svg"); break;
+        case 2: icon = SvgPicture.asset("../icons/seating.svg"); break;
+        case 3: icon = SvgPicture.asset("../icons/gardening.svg"); break;
+        case 4: icon = SvgPicture.asset("../icons/social.svg"); break;
+        case 5: icon = SvgPicture.asset("../icons/water.svg"); break;
+        case 6: icon = SvgPicture.asset("../icons/plants.svg"); break;
+        case 7: icon = SvgPicture.asset("../icons/shade.svg"); break; // TODO implement other
+        default: print("Could not assign type of suggestion");
+      }
       markers.add(
           Marker(
             width: 80.0,
@@ -23,7 +34,7 @@ class SuggestionManager {
             point: LatLng(suggestions[i].lat, suggestions[i].lng),
             builder: (ctx) =>
                 Container(
-                    child: Icon(Icons.location_pin)
+                    child: Icon(Icons.location_pin) // TODO get icons to work?
                 ))
           );
     }
@@ -36,7 +47,7 @@ class Suggestion {
 
   late String title;
   late String text;
-  late String type;
+  late int type;
   late double lat;
   late double lng;
 
