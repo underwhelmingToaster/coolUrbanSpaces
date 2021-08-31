a2ea772fedf54662c168934bae140354a8529a31
Fabio Zahner <zahnerfabio@gmail.com>
2021-08-31 18:34:03 +0200

    added first part of form

--- lib/fragments/addSuggestion.dart
+++ lib/fragments/addSuggestion.dart
@@ -3,11 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class StatefulAddSuggestionFragment extends StatefulWidget {
+  const StatefulAddSuggestionFragment({Key? key}) : super(key: key);
+
   @override
   _AddSuggestionFragment createState() => _AddSuggestionFragment();
 }
 
 class _AddSuggestionFragment extends State<StatefulAddSuggestionFragment> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,38 +21,55 @@ class _AddSuggestionFragment extends State<StatefulAddSuggestionFragment> {
           IconButton(onPressed: () { null; }, icon: Icon(Icons.add)) // TODO implement onpressed
         ],
       ),
-      body: Column(
-        children: <Widget> [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Title",
-              hintText: "A short title"
-            ),
-            inputFormatters: [LengthLimitingTextInputFormatter(50)],
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please add a title";
-              }
-              return null;
-            },
+      body: Form (
+        key: _formKey,
+        child: Padding(padding: EdgeInsets.all(10) ,
+          child: Column(
+            children: <Widget> [
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Title",
+                    hintText: "A short title"
+                ),
+                inputFormatters: [LengthLimitingTextInputFormatter(50)],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please add a title";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Description",
+                    hintText: "Describe your suggestion in more detail"
+                ),
+                inputFormatters: [LengthLimitingTextInputFormatter(500)],
+                minLines: 4,
+                maxLines: 10,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please describe your project";
+                  }
+                  return null;
+                },
+              ),
+              Padding(padding: EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              )
+            ],
           ),
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: "Description",
-                hintText: "Describe your suggestion in more detail"
-            ),
-            inputFormatters: [LengthLimitingTextInputFormatter(500)],
-            minLines: 4,
-            maxLines: 10,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please describe your project";
-              }
-              return null;
-            },
-          )
-        ],
-      ),
+        )
+      )
     );
   }
 
