1b9994dabe3fb220dc1b8f552c783d5b1b68a310
Felix Reiniger <felix.reiniger@ost.ch>
2021-09-10 08:39:23 +0200

    CLeanUp

--- lib/view/urban_map_view.dart
+++ lib/view/urban_map_view.dart
@@ -1,6 +1,5 @@
 import 'package:cool_urban_spaces/controller/add_suggestion_controller.dart';
 import 'package:cool_urban_spaces/controller/map_data_controller.dart';
-import 'package:cool_urban_spaces/fragments/viewSuggestion.dart';
 import 'package:cool_urban_spaces/view/add_suggestion_view.dart';
 import 'package:cool_urban_spaces/view/info_suggestion_view.dart';
 import 'package:flutter/cupertino.dart';
@@ -21,50 +20,53 @@ class UrbanMapView extends StatelessWidget{
   Widget build(BuildContext context) {
     AddSuggestionController suggestionController = Provider.of<AddSuggestionController>(context);
     MapDataController mapDataController = Provider.of<MapDataController>(context);
-    return new FlutterMap(
-      options: new MapOptions(
-        center: new LatLng(47.0, 8.0),
-        zoom: 13.0,
-        controller: mc,
-        plugins: [
-          MarkerClusterPlugin(),
-        ],
-        onTap: (point) {
-          suggestionController.lat = point.latitude;
-          suggestionController.lon = point.longitude;
-        },
-        onLongPress: (point) {
-          suggestionController.lat = point.latitude;
-          suggestionController.lon = point.longitude;
-          Navigator.push(context,
-              MaterialPageRoute(
-                  builder: (context) => AddSuggestionView()
-              )
-          );
-        },
-      ),
-      layers: [
-        new TileLayerOptions(
-          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          subdomains: ['a', 'b', 'c'],
-        ),
+    return FlutterMap(
+          options: new MapOptions(
+            center: new LatLng(47.0, 8.0),
+            zoom: 13.0,
+            controller: mc,
+            plugins: [
+              MarkerClusterPlugin(),
+            ],
+            onTap: (point) {
+              suggestionController.lat = point.latitude;
+              suggestionController.lon = point.longitude;
+              },
+            onLongPress: (point) {
+              suggestionController.lat = point.latitude;
+              suggestionController.lon = point.longitude;
+              Navigator.push(context,
+                  MaterialPageRoute(
+                      builder: (context) => AddSuggestionView()
+                  )
+              );
+              },
 
-        MarkerClusterLayerOptions(
-          markers: mapDataController.availableMarkers,
-          onMarkerTap: (value) {
-            int id = mapDataController.cleanUpKey(value.key as Key);
-            mapDataController.setSelectedMarkerToId(id);
-            Navigator.push(context, MaterialPageRoute(
-                builder: (context) => InfoSuggestionView()
-            ));
-          },
-          builder: (BuildContext context, List<Marker> markers) {
-            return FloatingActionButton(
-                child: Text(markers.length.toString()),
-                onPressed: () { }
-            );
-          },
-        ),]
-      );
-    }
+
+          ),
+          layers: [
+            new TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c'],
+            ),
+
+            MarkerClusterLayerOptions(
+              markers: mapDataController.availableMarkers,
+              onMarkerTap: (value) {
+                int id = mapDataController.cleanUpKey(value.key as Key);
+                mapDataController.setSelectedMarkerToId(id);
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => InfoSuggestionView()
+                ));
+                },
+              builder: (BuildContext context, List<Marker> markers) {
+                return FloatingActionButton(
+                    child: Text(markers.length.toString()),
+                    onPressed: () { }
+                    );
+                },
+            ),
+          ]
+    );
+  }
 }
