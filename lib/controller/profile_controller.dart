c90cbdaeffe5e36d7a7158e1ad14b3809fa27a1d
Felix Reiniger <felix.reiniger@ost.ch>
2021-09-13 10:59:46 +0200

    Implement password check

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,12 +1,40 @@
+import 'package:crypt/crypt.dart';
 import 'package:flutter/cupertino.dart';
 
 class ProfileController extends ChangeNotifier {
+  final String salt = "I'm very salty";
+
   String _username = "";
+
+  List<String> _passwords = ["", ""];
 
   String get username => _username;
 
   set username(String value) {
     _username = value;
     notifyListeners();
+  }
+
+  set password(String value) {
+    _passwords[0] = value;
+    notifyListeners();
+  }
+
+  set scndpassword(String value) {
+    _passwords[1] = value;
+    notifyListeners();
+  }
+
+  bool submit(){
+    String passwordHash = Crypt.sha256(_passwords[0], rounds: 1000, salt: salt).hash.toString();
+    String passwordHash2 = Crypt.sha256(_passwords[1], rounds: 1000, salt: salt).hash.toString();
+    if(username.isNotEmpty && passwordHash.isNotEmpty && passwordHash2.isNotEmpty) {
+      if (passwordHash2 == passwordHash) {
+        // TODO: Further process password and username
+        return true;
+      }
+    }
+
+    return false;
   }
 }
--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:cool_urban_spaces/controller/profile_controller.dart';
+import 'package:crypt/crypt.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -15,13 +16,89 @@ class ProfileView extends StatelessWidget {
       body: ListView(
         children: [
           Card(
-            child: ListTile(
-              title: Text("Username"),
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Username"),
+                          TextFormField(
+                            maxLength: 20,
+                            onChanged: (value) {
+                              profileController.username = value;
+                            },
+                          )
+                        ],
+                      )
+                  )
+                ],
+              ),
             ),
           ),
           Card(
-            child: ListTile(
-              title: Text("Password"),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Password"),
+                            Padding(
+                              padding: EdgeInsets.all(8),
+                              child: TextFormField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                onChanged: (value) {
+                                  profileController.password = value;
+                                },
+                              ),
+                            ),
+                            Text("Repeat Password"),
+                            Padding(
+                              padding: EdgeInsets.all(8),
+                              child: TextFormField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                onChanged: (value) {
+                                  profileController.scndpassword = value;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(30),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                    ),
+                    key: Key("Submit Profile-Form"),
+                    onPressed: () {
+                      if(profileController.submit()){
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Profile details changed')));
+                      }else{
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Password incorrect or fields empty'), backgroundColor: Colors.red));
+                      }
+                    },
+                    child: Text("Change Profile"),
+                  ),
+                )
+              ],
             ),
           )
         ],
