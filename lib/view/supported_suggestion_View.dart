85f1b3ab04396b040d3a19f0a30b7646515da7b5
Felix Reiniger <felix.reiniger@ost.ch>
2021-11-05 10:47:04 +0100

    Implement SuggestionView

---
+++ lib/view/supported_suggestion_View.dart
@@ -0,0 +1,54 @@
+
+import 'package:cool_urban_spaces/controller/map_data_controller.dart';
+import 'package:cool_urban_spaces/controller/settings_controller.dart';
+import 'package:cool_urban_spaces/model/suggestion.dart';
+import 'package:cool_urban_spaces/view/widgets/suggestion_list_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BrowsingView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    MapDataController dataController = Provider.of<MapDataController>(context);
+    SettingsController settingsController = Provider.of<SettingsController>(
+        context);
+
+    List<SuggestionModel> suggestions = dataController.cachedSuggestions;
+    List<Widget> widgets = [];
+
+    suggestions = dataController.getSortedSuggestions(settingsController.browsingSort); //FIXME: only get supported suggestions (adjust data)
+
+    suggestions.forEach((suggestion) {
+      widgets.add(new Card(
+        child: ListTile(
+          onTap: () {},
+          title: Text(suggestion.title),
+          trailing: dataController.getMarkerIcon(suggestion),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(suggestion.text),
+            ],
+          ),
+        ),
+      ));
+    });
+
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme
+                .of(context)
+                .primaryColor,
+            title: Text("Browse"),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            ],
+          ),
+          body: SuggestionList(suggestions),
+        )
+    );
+  }
+}
