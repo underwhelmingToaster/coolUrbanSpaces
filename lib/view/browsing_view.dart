2c40174391e56755d4364ed80f4d53e48b545ed2
Felix Reiniger <felix.reiniger@ost.ch>
2021-11-05 11:07:01 +0100

    Remove unused variable and optimize imports

--- lib/view/browsing_view.dart
+++ lib/view/browsing_view.dart
@@ -1,9 +1,7 @@
 import 'package:cool_urban_spaces/controller/map_data_controller.dart';
 import 'package:cool_urban_spaces/controller/settings_controller.dart';
-import 'package:cool_urban_spaces/controller/enum/sorting_type.dart';
 import 'package:cool_urban_spaces/model/suggestion.dart';
 import 'package:cool_urban_spaces/view/widgets/suggestion_list_widget.dart';
-import 'package:cool_urban_spaces/view/widgets/utils_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
--- lib/view/widgets/suggestion_list_widget.dart
+++ lib/view/widgets/suggestion_list_widget.dart
@@ -1,13 +1,10 @@
 import 'package:cool_urban_spaces/controller/enum/sorting_type.dart';
-import 'package:cool_urban_spaces/controller/map_data_controller.dart';
 import 'package:cool_urban_spaces/controller/settings_controller.dart';
 import 'package:cool_urban_spaces/model/suggestion.dart';
 import 'package:cool_urban_spaces/view/widgets/utils_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../info_suggestion_view.dart';
 
 class SuggestionList extends StatelessWidget{
 
@@ -19,7 +16,7 @@ class SuggestionList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     SettingsController settingsController = Provider.of<SettingsController>(context);
-    MapDataController dataController = Provider.of<MapDataController>(context); //TODO: try to remove this from here
+
     List<Widget> widgets = [];
 
     suggestions.forEach((suggestion) {
